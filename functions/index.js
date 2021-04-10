0b4c8ec69c147c0af1ed5ff340c99649b39dfc16
Matt <mpickle86@gmail.com>
2021-04-10 18:15:01 -0500

    Fix bugs and add error handling

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,46 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const fetch = require("node-fetch");
 admin.initializeApp();
 const db = admin.firestore().collection("Weather Data Cache");
 
 //Accept weather data request from client
-exports.requestData = functions.https.onCall((data, context) => {
+exports.requestData = functions.https.onCall(async (data, context) => {
   //Check firestore database for recent data from that city
-  const cityCodeRef = db.doc(data.cityCode);
-  const now = new Date();
-  if (now - cityCodeRef.timestamp < 900000) {
+  const cityData = await db.doc(data.cityCode).get();
+  const now = Date.now();
+  if (cityData.exists && now - cityData.data().timestamp < 900000) {
     //Send cached data to client
-    return cityCodeRef;
+    return cityData.data();
   } else {
     //Request data from API
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?
-      lat=${data.latitude}&
-      lon=${data.longitude}&
-      units=${data.units}&
-      appid=826e7f95db9392bcb8f4179da8c00a4e`)
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.latitude}&lon=${data.longitude}&units=${data.units}&appid=826e7f95db9392bcb8f4179da8c00a4e`)
       .then(res => {
         if (res.ok) {
-          res.json().then(newWeatherObj => {
-            //Save data to database with new timestamp
-            const newData = {
-              cityName: data.cityName,
-              latitude: data.latitude,
-              longitude: data.longitude,
-              units: data.units,
-              timestamp: now,
-              weatherObj: newWeatherObj
-            };
-            cityCodeRef.set(newData);
-            //Send data to client
-            return newData;
-          });
+          return res.json()
         } else {
-          console.error("API Request Failed!");
+          throw new Error("API Request Failed!");
         }
-      });
-  }
+      })
+      .then(newWeatherObj => {
+        //Save data to database with new timestamp
+        const newData = {
+          cityName: data.cityName,
+          latitude: data.latitude,
+          longitude: data.longitude,
+          units: data.units,
+          timestamp: now,
+          weatherObj: newWeatherObj
+        };
+        db.doc(data.cityCode).set(newData);
+        //Send data to client
+        return newData;
+      })
+      .catch(err => {
+        console.error(err.message);
+        return err.message;
+      })
+  }    
 });
 
   
