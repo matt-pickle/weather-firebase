3a3f162182f010bb8869f532bb391b4fce3f0e47
Matt <mpickle86@gmail.com>
2021-04-11 16:59:14 -0500

    Refactor to onRequest call

--- functions/index.js
+++ functions/index.js
@@ -5,40 +5,37 @@ admin.initializeApp();
 const db = admin.firestore().collection("Weather Data Cache");
 
 //Accept weather data request from client
-exports.requestData = functions.https.onCall(async (data) => {
+exports.requestData = functions.https.onRequest(async (req, res) => {
   //Check firestore database for recent data from that city
-  const cityData = await db.doc(data.cityCode).get();
+  const cityData = await db.doc(req.body.data.cityCode).get();
   const now = Date.now();
   if (cityData.exists && now - cityData.data().timestamp < 900000) {
     //Send cached data to client
-    return cityData.data();
+    res.send(cityData.data());
   } else {
     //Request data from API
-    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.latitude}&lon=${data.longitude}&units=${data.units}&appid=${functions.config().openweather.key}`)
-      .then(res => {
-        if (res.ok) {
-          return res.json()
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${req.body.data.latitude}&lon=${req.body.data.longitude}&units=${req.body.data.units}&appid=${functions.config().openweather.key}`)
+      .then(response => {
+        if (response.ok) {
+          response.json().then(newWeatherObj => {
+            const newData = {
+              cityName: req.body.data.cityName,
+              latitude: req.body.data.latitude,
+              longitude: req.body.data.longitude,
+              units: req.body.data.units,
+              timestamp: now,
+              weatherObj: newWeatherObj
+            };
+            db.doc(req.body.data.cityCode).set(newData);
+            res.send(newData);
+          })
         } else {
           throw new Error("API Request Failed!");
         }
       })
-      .then(newWeatherObj => {
-        //Save data to database with new timestamp
-        const newData = {
-          cityName: data.cityName,
-          latitude: data.latitude,
-          longitude: data.longitude,
-          units: data.units,
-          timestamp: now,
-          weatherObj: newWeatherObj
-        };
-        db.doc(data.cityCode).set(newData);
-        //Send data to client
-        return newData;
-      })
       .catch(err => {
         console.error(err.message);
-        return err.message;
+        res.status(500).send(err.message);
       })
   }    
 });
